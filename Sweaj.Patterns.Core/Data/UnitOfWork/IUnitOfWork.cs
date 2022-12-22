eafd16ab4d485db34552e625ee856351adfee164
Anthony J <reapsprgm@gmail.com>
2022-12-22 12:58:35 -0500

    Data updates to Unit of work and data context

---
+++ Sweaj.Patterns.Core/Data/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using Sweaj.Patterns.Data.Entities;
+using Sweaj.Patterns.Data.Services;
+
+namespace Sweaj.Patterns.Data.UnitOfWork
+{
+    public interface IUnitOfWork : IRepositoryRetriever
+    {
+        Task CommitAsync();
+        Task Rollback();
+    }
+
+    public interface IUnitOfWork<TEntity> : IRepositoryRetriever<TEntity>
+        where TEntity : Entity, IAggregateRoot
+    {
+        Task CommitAsync();
+        Task Rollback();
+    }
+
+    public interface IUnitOfWork<TKey, TEntity> : IRepositoryRetriever<TKey, TEntity>
+        where TKey : IEquatable<TKey>, new()
+        where TEntity : Entity<TKey>, IAggregateRoot
+    {
+        Task CommitAsync();
+        Task Rollback();
+    }
+}
