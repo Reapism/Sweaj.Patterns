08e251d62c7d3288b9a81c141dddb9280fa52d9f
Anthony J <reapsprgm@gmail.com>
2022-12-22 12:56:28 -0500

    Reflection extensions

---
+++ Sweaj.Patterns.Core/Reflection/ReflectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Sweaj.Patterns.Reflection
+{
+    public static class ReflectionExtensions
+    {
+        public static FieldInfo[] GetFieldsBy<T>(BindingFlags bindingAttrs, Func<FieldInfo, bool> filter)
+        {
+            return typeof(T).GetFields(bindingAttrs).Where(filter).ToArray();
+        }
+
+        public static PropertyInfo[] GetPropertiesBy<T>(BindingFlags bindingAttrs, Func<PropertyInfo, bool> filter)
+        {
+            return typeof(T).GetProperties(bindingAttrs).Where(filter).ToArray();
+        }
+
+        public static MemberInfo[] GetMembersBy<T>(BindingFlags bindingAttrs, Func<MemberInfo, bool> filter)
+        {
+            return typeof(T).GetMembers(bindingAttrs).Where(filter).ToArray();
+        }
+    }
+
+
+}
