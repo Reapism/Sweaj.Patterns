1d063ec1a12e93310ab73666dea0a62b6db1f7d5
Anthony J <reapsprgm@gmail.com>
2022-12-22 12:57:28 -0500

    Options provider

---
+++ Sweaj.Patterns.Core/Options/IOptions.cs
@@ -0,0 +1,12 @@
+﻿namespace Sweaj.Patterns.Options
+{
+    public interface IOptions<T>
+    {
+        void Configure(Action<T> options);
+    }
+
+    public interface IAsyncOptions<T>
+    {
+        Task ConfigureAsync(Task<Action<T>> options);
+    }
+}
