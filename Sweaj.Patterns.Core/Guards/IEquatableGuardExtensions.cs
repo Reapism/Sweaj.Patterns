24de130ef4874addf8a075e0432e368190a59397
Anthony J <reapsprgm@gmail.com>
2022-12-27 01:32:48 -0500

    Equatable Guard Extensions

--- Sweaj.Patterns.Core/Guards/IEquatableGuardExtensions.cs
+++ Sweaj.Patterns.Core/Guards/IEquatableGuardExtensions.cs
@@ -1,21 +1,10 @@
-﻿using Ardalis.GuardClauses;
-
-namespace Sweaj.Patterns.Guards
+﻿namespace Sweaj.Patterns.Guards
 {
 
-    public static class GuardExtensions
+    public static class IEquatableGuardExtensions
     {
         public static void Equals<T>(this IGuardClause guardClause, T expectedInput, T actualInput, string actualInputParameterName)
             where T : IEquatable<T>
-        {
-            if (expectedInput.Equals(actualInput))
-            {
-                throw new ArgumentException("Actual input was equal to the expected input.", actualInputParameterName);
-            }
-        }
-
-        public static void Equals<T, W>(this IGuardClause guardClause, T expectedInput, T actualInput, string actualInputParameterName)
-            where T : W, IEquatable<T>
         {
             if (expectedInput.Equals(actualInput))
             {
