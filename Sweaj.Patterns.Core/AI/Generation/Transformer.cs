acf57b31492fa346b99e5c2e5d4304c3834524dc
Anthony J <reapsprgm@gmail.com>
2023-01-02 23:56:52 -0500

    beginning of basic AI impl

---
+++ Sweaj.Patterns.Core/AI/Generation/Transformer.cs
@@ -0,0 +1,21 @@
+﻿//using Sweaj.Patterns.AI.Attributes;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Reflection;
+//using System.Text;
+//using System.Threading.Tasks;
+
+namespace Sweaj.Patterns.AI.Generation
+{
+    public struct Transformer
+    {
+        [Gene]
+        public TResult Transform<T, TResult>(Func<T, TResult> input, Func<Func<T, TResult>, TResult> computer)
+        {
+            var result = computer(input);
+            return result;
+        }
+
+    }
+}
