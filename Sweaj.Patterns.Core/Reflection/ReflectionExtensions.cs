abc472bea67f3f3c4a00913ca89fc86195571153
Anthony J <reapsprgm@gmail.com>
2023-01-02 23:58:30 -0500

    Updates to reflection extensions to return the sequence without enumerating it.

--- Sweaj.Patterns.Core/Reflection/ReflectionExtensions.cs
+++ Sweaj.Patterns.Core/Reflection/ReflectionExtensions.cs
@@ -5,19 +5,19 @@ namespace Sweaj.Patterns.Reflection
 {
     public static class ReflectionExtensions
     {
-        public static FieldInfo[] GetFieldsBy<T>(BindingFlags bindingAttrs, Func<FieldInfo, bool> filter)
+        public static IEnumerable<FieldInfo> GetFieldsBy<T>(BindingFlags bindingAttrs, Func<FieldInfo, bool> filter)
         {
-            return typeof(T).GetFields(bindingAttrs).Where(filter).ToArray();
+            return typeof(T).GetFields(bindingAttrs).Where(filter);
         }
 
-        public static PropertyInfo[] GetPropertiesBy<T>(BindingFlags bindingAttrs, Func<PropertyInfo, bool> filter)
+        public static IEnumerable<PropertyInfo> GetPropertiesBy<T>(BindingFlags bindingAttrs, Func<PropertyInfo, bool> filter)
         {
-            return typeof(T).GetProperties(bindingAttrs).Where(filter).ToArray();
+            return typeof(T).GetProperties(bindingAttrs).Where(filter);
         }
 
-        public static MemberInfo[] GetMembersBy<T>(BindingFlags bindingAttrs, Func<MemberInfo, bool> filter)
+        public static IEnumerable<MemberInfo> GetMembersBy<T>(BindingFlags bindingAttrs, Func<MemberInfo, bool> filter)
         {
-            return typeof(T).GetMembers(bindingAttrs).Where(filter).ToArray();
+            return typeof(T).GetMembers(bindingAttrs).Where(filter);
         }
     }
 
