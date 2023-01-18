969025e7f88286c85b766d5c9710ee469eb32f17
Anthony J <reapsprgm@gmail.com>
2023-01-17 22:54:53 -0500

    Introduce some cache integration tests. completing once near finalizing cache infrastructure

---
+++ Sweaj.Patterns.Tests/Cache/CacheIntegrationTests.cs
@@ -0,0 +1,37 @@
+using Ardalis.GuardClauses;
+using Microsoft.Extensions.Caching.Memory;
+using Sweaj.Patterns.Cache;
+
+namespace Sweaj.Patterns.Tests.Cache
+{
+    public sealed class RandomValue : ICacheKeyConstructor
+    {
+        public RandomValue(string value)
+        {
+            Value = Guard.Against.NullOrWhiteSpace(value);
+        }
+        public string Value { get; }
+
+        public CacheKey CreateCacheKey()
+        {
+            return CacheKey.Create("|", nameof(RandomValue), Value);
+        }
+    }
+
+    public class CacheIntegrationTests
+    {
+        [Fact]
+        public void CacheWorksE2E()
+        {
+            var randomValue = new RandomValue(Random.Shared.Next().ToString());
+            var cacheManager = new CacheManager(IMemoryCache)
+            var randomValueStore = CacheRequest<RandomValue>.SetCacheOnly(randomValue, CacheDurationOptions.FromRelativeToNowExpiration(TimeSpan.FromMinutes(1))).Get();
+
+
+
+        }
+
+
+
+    }
+}
