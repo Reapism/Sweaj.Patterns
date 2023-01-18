a7e84f6b8f7cb549d9a8b687668fcda64b743991
Anthony J <reapsprgm@gmail.com>
2023-01-17 22:53:21 -0500

    ValueResultStatus

---
+++ Sweaj.Patterns.Core/Data/Statuses/ValueResultStatus.cs
@@ -0,0 +1,13 @@
+﻿namespace Sweaj.Patterns.Data.Statuses
+{
+    public enum ValueResultStatus : byte
+    {
+        Empty = 0,
+        Value = 1,
+        Cache = 2,
+        ValueFactoryFromCache = 3,
+        DataStore = 4,
+        ValueFactoryFromDataStore = 5,
+        ThirdParty = 6
+    }
+}
