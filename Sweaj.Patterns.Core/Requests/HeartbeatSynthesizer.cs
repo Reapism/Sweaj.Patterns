0a3af746b4d5948640605759a20ed41e5e5593ae
Anthony J <reapsprgm@gmail.com>
2023-01-17 22:52:30 -0500

    introduce heartbeat

---
+++ Sweaj.Patterns.Core/Requests/HeartbeatSynthesizer.cs
@@ -0,0 +1,20 @@
+﻿namespace Sweaj.Patterns.Requests
+{
+    public sealed class HeartbeatSynthesizer
+    {
+        private HeartbeatSynthesizer(HeartbeatStatus heartbeatStatus) 
+        {
+            RequestId = Guid.NewGuid();
+            Status = heartbeatStatus;
+        }
+
+        public Guid RequestId { get; }
+        public HeartbeatStatus Status { get; }
+
+        public static async Task<HeartbeatSynthesizer> CreateAsync<TParam>(Func<TParam, Task<HeartbeatStatus>> heartbeatCall, TParam param)
+        {
+            var heartbeatStatus = await heartbeatCall(param);
+            return new HeartbeatSynthesizer(heartbeatStatus);
+        }
+    }
+}
