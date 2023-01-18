e0fcc05585b807407da03f2227a0ffbf97317094
Anthony J <reapsprgm@gmail.com>
2023-01-17 22:54:00 -0500

    AI modeling?

---
+++ Sweaj.Patterns.Core/AI/IModel.cs
@@ -0,0 +1,17 @@
+﻿using Sweaj.Patterns.Data.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweaj.Patterns.AI
+{
+    /// <summary>
+    /// Marks a type as being an AI Model.
+    /// </summary>
+    public interface IModel
+    {
+
+    }
+}
