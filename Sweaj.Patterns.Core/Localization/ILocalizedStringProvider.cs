c6eccab434f043390aed9bcc1a9adc047d6221a2
Anthony J <reapsprgm@gmail.com>
2023-02-08 00:12:42 -0500

    localization

---
+++ Sweaj.Patterns.Core/Localization/ILocalizedStringProvider.cs
@@ -0,0 +1,9 @@
+﻿using System.Globalization;
+
+namespace Sweaj.Patterns.Localization
+{
+    public interface ILocalizedStringProvider
+    {
+        string GetString(string key, CultureInfo culture);
+    }
+}
