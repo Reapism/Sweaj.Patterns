d06efc7ea0746ce6540a41d367456c5b99b41b87
Anthony J <reapsprgm@gmail.com>
2023-02-24 16:38:51 -0500

    Add FullName

---
+++ src/Sweaj.Patterns/Data/ValueObjects/FullName.cs
@@ -0,0 +1,29 @@
+﻿namespace Sweaj.Patterns.Data.ValueObjects
+{
+    /// <summary>
+    /// An object that represents a fully constructed name that is validated upon creation .
+    /// </summary>
+    public class FullName
+    {
+        public string FirstName { get; }
+        public string? MiddleName { get; }
+        public string LastName { get; }
+
+        public FullName(string firstName, string lastName)
+            : this(firstName, string.Empty, lastName)
+        {
+        }
+
+        public FullName(string firstName, string? middleName, string lastName)
+        {
+            FirstName = Guard.Against.NullOrWhiteSpace(firstName);
+            MiddleName = middleName ?? string.Empty;
+            LastName = Guard.Against.NullOrWhiteSpace(lastName);
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {(string.IsNullOrEmpty(MiddleName) ? string.Empty : $"{MiddleName} ")}{LastName}";
+        }
+    }
+}
