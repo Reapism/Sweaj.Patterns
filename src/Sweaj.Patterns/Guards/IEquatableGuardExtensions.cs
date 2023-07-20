3c6ad6b342d57ceaf93776a4d05b72a4ad2d53cc
Anthony J <reapsprgm@gmail.com>
2023-07-20 12:38:16 -0400

    Add new custom guard extensions

--- src/Sweaj.Patterns/Guards/IEquatableGuardExtensions.cs
+++ src/Sweaj.Patterns/Guards/IEquatableGuardExtensions.cs
@@ -1,8 +1,27 @@
-﻿namespace Sweaj.Patterns.Guards
+﻿using Sweaj.Patterns.NullObject;
+using System.Runtime.CompilerServices;
+
+namespace Sweaj.Patterns.Guards
 {
 
     public static class IEquatableGuardExtensions
     {
+        public static T NullOrEmpty<T>(this IGuardClause guardClause, T value, [CallerMemberName]string parameterName = "")
+            where T : IEmpty
+        {
+            if (value is null)
+            {
+                throw new ArgumentNullException(parameterName, "The provided value is null.");
+            }
+
+            if (value.IsEmpty())
+            {
+                throw new ArgumentException("The provided value is empty.");
+            }
+
+            return value;
+        }
+
         public static void Equals<T>(this IGuardClause guardClause, T expectedInput, T actualInput, string actualInputParameterName)
             where T : IEquatable<T>
         {
