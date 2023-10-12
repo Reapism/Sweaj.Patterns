6c9dc44b9dabac6880f8622f1ca9f815b9255a1d
Anthony J <reapsprgm@gmail.com>
2023-10-11 21:28:14 -0400

    Entity changes

--- src/Sweaj.Patterns/Data/Entities/AuditableEntity.cs
+++ src/Sweaj.Patterns/Data/Entities/AuditableEntity.cs
@@ -1,18 +1,28 @@
 ﻿namespace Sweaj.Patterns.Data.Entities
 {
-    public interface IAuditableEntity
+    internal interface IAuditableEntity
     {
-        DateTimeOffset LastModifiedDate { get; set; }
-        string LastModifiedByUser { get; set; }
-        DateTimeOffset CreatedDate { get; set; }
-        string CreatedByUser { get; set; }
+        DateTimeOffset LastModifiedDate { get; }
+        string LastModifiedByUser { get; }
+        DateTimeOffset CreatedDate { get; }
+        string CreatedByUser { get; }
     }
 
     public abstract class AuditableEntity : Entity, IAuditableEntity
     {
-        public DateTimeOffset LastModifiedDate { get; set; }
-        public string LastModifiedByUser { get; set; } = string.Empty;
-        public DateTimeOffset CreatedDate { get; set; }
-        public string CreatedByUser { get; set; } = string.Empty;
+        public DateTimeOffset LastModifiedDate { get; private set; }
+        public string LastModifiedByUser { get; private set; } = string.Empty;
+        public DateTimeOffset CreatedDate { get; private set; }
+        public string CreatedByUser { get; private set; } = string.Empty;
+
+        public void Audit(string modifyingUser)
+        {
+            LastModifiedDate = DateTimeOffset.Now;
+        }
+
+        public void AuditNewUserModified(string modifyingUser)
+        {
+
+        }
     }
 }
