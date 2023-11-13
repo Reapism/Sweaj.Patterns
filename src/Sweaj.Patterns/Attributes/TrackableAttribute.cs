7e7cf283634a9b89700768599940071116d1e3de
Anthony J <reapsprgm@gmail.com>
2023-11-12 22:12:38 -0500

    Update name, its called Trackable

---
+++ src/Sweaj.Patterns/Attributes/TrackableAttribute.cs
@@ -0,0 +1,7 @@
+﻿namespace Sweaj.Patterns.Attributes
+{
+    [AttributeUsage(AttributeTargets.Interface, AllowMultiple = false, Inherited = true)]
+    public class TrackableAttribute : Attribute
+    {
+    }
+}
