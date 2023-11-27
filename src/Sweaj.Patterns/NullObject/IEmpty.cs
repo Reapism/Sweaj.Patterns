a5e4596da2b19a1cd0b03d712b312ab583b41e03
Anthony J <reapsprgm@gmail.com>
2023-11-27 00:45:13 -0500

    Slight updates to empty pattern, non breaking
    - Add comments, and nameof

--- src/Sweaj.Patterns/NullObject/IEmpty.cs
+++ src/Sweaj.Patterns/NullObject/IEmpty.cs
@@ -1,5 +1,8 @@
 ﻿namespace Sweaj.Patterns.NullObject
 {
+    /// <summary>
+    /// A mechanism for checking if a type has an empty state.
+    /// </summary>
     public interface IEmpty
     {
         bool IsEmpty();
