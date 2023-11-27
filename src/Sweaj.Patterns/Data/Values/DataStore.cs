eded82b935a76d6ed6ea578253e4e4e2d85300b7
Anthony J <reapsprgm@gmail.com>
2023-11-27 00:43:54 -0500

    Update Values

---
+++ src/Sweaj.Patterns/Data/Values/DataStore.cs
@@ -0,0 +1,103 @@
+﻿using Sweaj.Patterns.Data.Entities;
+using System.Diagnostics;
+
+namespace Sweaj.Patterns.Data.Values
+{
+    public class DataStore<TKey, TEntity> : IValueProvider<TEntity>
+        where TKey : IEquatable<TKey>, new()
+        where TEntity : Entity<TKey>
+    {
+        private DataStore(TEntity value, TimeSpan duration, ValueResultStatus valueResultStatus)
+        {
+            Value = Guard.Against.Null(value, nameof(value));
+            Duration = duration;
+            Status = valueResultStatus;
+        }
+
+        public TEntity Value { get; } = default;
+
+        public bool HasValue => Value is not null;
+        public ValueResultStatus Status { get; }
+        TimeSpan Duration { get; }
+
+        public static DataStore<TKey, TEntity> FromDatastore(
+            Func<TEntity> getValueDelegate)
+        {
+            return From(getValueDelegate, ValueResultStatus.ValueFactoryFromDataStore);
+        }
+
+        public static async Task<DataStore<TKey, TEntity>> FromDatabaseAsync(
+            Func<Task<TEntity>> getValueDelegate)
+        {
+            return await FromAsync(getValueDelegate, ValueResultStatus.ValueFactoryFromDataStore);
+        }
+
+        public static DataStore<TKey, TEntity> FromCache(
+            Func<TEntity> getValueDelegate)
+        {
+            return From(getValueDelegate, ValueResultStatus.Cache);
+        }
+
+        public static async Task<DataStore<TKey, TEntity>> FromCacheAsync(
+            Func<Task<TEntity>> getValueDelegate)
+        {
+            return await FromAsync(getValueDelegate, ValueResultStatus.ValueFactoryFromCache);
+        }
+
+        public static DataStore<TKey, TEntity> FromFile(
+            Func<TEntity> getValueDelegate)
+        {
+            return From(getValueDelegate, ValueResultStatus.File);
+        }
+
+        public static async Task<DataStore<TKey, TEntity>> FromFileAsync(Func<Task<TEntity>> getValueDelegate)
+        {
+            return await FromAsync(getValueDelegate, ValueResultStatus.File);
+        }
+
+        public static DataStore<TKey, TEntity> FromWebResource(Func<TEntity> getValueDelegate)
+        {
+            return From(getValueDelegate, ValueResultStatus.WebResource);
+        }
+
+        public static async Task<DataStore<TKey, TEntity>> FromWebResourceAsync(Func<Task<TEntity>> getValueDelegate)
+        {
+            return await FromAsync(getValueDelegate, ValueResultStatus.WebResource);
+        }
+
+        private static DataStore<TKey, TEntity> From(
+            Func<TEntity> getValueDelegate, ValueResultStatus dataRetrievalStatus)
+        {
+            var time = Stopwatch.StartNew();
+
+            var value = getValueDelegate();
+
+            time.Stop();
+            var duration = time.Elapsed;
+
+            var dataStore = new DataStore<TKey, TEntity>(value, duration, dataRetrievalStatus);
+
+            return dataStore;
+        }
+
+        private static async Task<DataStore<TKey, TEntity>> FromAsync(
+            Func<Task<TEntity>> getValueDelegate, ValueResultStatus dataRetrievalStatus)
+        {
+            var startTime = Stopwatch.GetTimestamp();
+
+            var value = await getValueDelegate();
+
+            var endTime = Stopwatch.GetTimestamp();
+            var duration = Stopwatch.GetElapsedTime(startTime, endTime);
+
+            var dataStore = new DataStore<TKey, TEntity>(value, duration, dataRetrievalStatus);
+
+            return dataStore;
+        }
+
+        public ValueStore<TEntity> AsValueStore()
+        {
+            return ValueStore<TEntity>.FromDataStore(this);
+        }
+    }
+}
---
+++ src/Sweaj.Patterns/Data/Values/ValueResultStatus.cs
@@ -0,0 +1,14 @@
+﻿namespace Sweaj.Patterns.Data.Values
+{
+    public enum ValueResultStatus : byte
+    {
+        Empty = 0,
+        Value = 1,
+        Cache = 2,
+        DataStore = 3,
+        WebResource = 4,
+        ValueFactoryFromCache = 5,
+        ValueFactoryFromDataStore = 6,
+        File = 7
+    }
+}
