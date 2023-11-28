effdbc814e4706fbdec9d6a1d5b4b354005d098d
Anthony J <reapsprgm@gmail.com>
2023-11-27 21:17:30 -0500

    Update cache infra to use non generic json serializer

--- src/Sweaj.Patterns/Cache/CacheManagerBase.cs
+++ src/Sweaj.Patterns/Cache/CacheManagerBase.cs
@@ -5,6 +5,6 @@ namespace Sweaj.Patterns.Cache
     public abstract class CacheManagerBase
     {
         public abstract Task ProcessAsync<TValue>(CacheReadOnlyRequest cacheRequest, CancellationToken cancellationToken = default);
-        public abstract Task<CacheStore<TValue>> ProcessWithValueAsync<TValue>(CacheValueRequest<TValue> cacheRequest, IJsonSerializer<TValue> serializer, CancellationToken cancellationToken = default);
+        public abstract Task<CacheStore<TValue>> ProcessWithValueAsync<TValue>(CacheValueRequest<TValue> cacheRequest, IJsonSerializer serializer, CancellationToken cancellationToken = default);
     }
 }
--- src/Sweaj.Patterns/Cache/CacheRequestExtensions.cs
+++ src/Sweaj.Patterns/Cache/CacheRequestExtensions.cs
@@ -4,7 +4,7 @@ namespace Sweaj.Patterns.Cache
 {
     public static class CacheRequestExtensions
     {
-        public static async Task<CacheStore<TValue>> GetFromCacheOnly<TValue>(this CacheKey cacheKey, CacheManagerBase cacheManager, IJsonSerializer<TValue> jsonSerializer, CancellationToken cancellationToken = default)
+        public static async Task<CacheStore<TValue>> GetFromCacheOnly<TValue>(this CacheKey cacheKey, CacheManagerBase cacheManager, IJsonSerializer jsonSerializer, CancellationToken cancellationToken = default)
         {
             var request = CacheValueRequest<TValue>.GetFromCacheOnly(cacheKey);
             return await cacheManager.ProcessWithValueAsync(request, jsonSerializer, cancellationToken);
