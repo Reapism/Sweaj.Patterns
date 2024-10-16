c1c3c500894c9c0883bdf4ea7ab72d85fb1f8a73
Anthony J <reapsprgm@gmail.com>
2024-10-16 18:50:35 -0400

    Add new AuditableEntity to support custom key

--- src/Sweaj.Patterns/Data/Entities/AuditableEntity.cs
+++ src/Sweaj.Patterns/Data/Entities/AuditableEntity.cs
@@ -32,4 +32,31 @@ namespace Sweaj.Patterns.Data.Entities
             Audit(createdByUser);
         }
     }
+    /// <summary>
+    /// Marks a type as being an entity with a custom key type with audit properties.
+    /// <para>Use this abstraction if you want specify a different key.</para>
+    /// </summary>
+    /// <typeparam name="TKey"></typeparam>
+    [Trackable]
+    public abstract class AuditableEntity<TKey> : Entity<TKey>, IAuditableEntity
+        where TKey : IEquatable<TKey>, new()
+    {
+        public DateTimeOffset LastModifiedDate { get; private set; } = DateTimeOffset.UtcNow;
+        public string LastModifiedByUser { get; private set; } = string.Empty;
+        public DateTimeOffset CreatedDate { get; private set; } = DateTimeOffset.UtcNow;
+        public string CreatedByUser { get; private set; } = string.Empty;
+
+        public void Audit(string modifyingUser)
+        {
+            LastModifiedDate = DateTimeOffset.UtcNow;
+            LastModifiedByUser = modifyingUser;
+        }
+
+        public void AuditNewUserModified(string createdByUser)
+        {
+            CreatedByUser = createdByUser;
+            CreatedDate = DateTimeOffset.UtcNow;
+            Audit(createdByUser);
+        }
+    }
 }
